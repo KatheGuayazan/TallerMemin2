8479b60bb124047547e06dc24a1d48162c6af16d
Sebastian Villamil <162932132+sebas64mil@users.noreply.github.com>
2026-05-16 23:08:07 -0500

    Remove comment in PlayerInputHandler
    Removed comment about Unity Input System.

--- Assets/MemoEngineer/Scripts/Player/PlayerInputHandler.cs
+++ Assets/MemoEngineer/Scripts/Player/PlayerInputHandler.cs
@@ -5,7 +5,6 @@ public class PlayerInputHandler : MonoBehaviour
 {
     public float MoveInput { get; private set; }
 
-    // ------------------ Called by Unity Input System ------------------
     public void OnMove(InputAction.CallbackContext context)
     {
         MoveInput = context.ReadValue<float>();
