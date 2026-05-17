21974ee8c7f1d8e3ddc8ce7e929fabfe1c797d4e
Sebastian Villamil <162932132+sebas64mil@users.noreply.github.com>
2026-05-16 23:08:56 -0500

    Clean up comments in CollectibleObject.cs
    Removed comments for better code clarity.

--- Assets/MemoEngineer/Scripts/Systems/MechanicisGame/CollectibleObject.cs
+++ Assets/MemoEngineer/Scripts/Systems/MechanicisGame/CollectibleObject.cs
@@ -15,13 +15,11 @@ public class CollectibleObject : MonoBehaviour, IPlayerCollision, IPoolable
     }
 
 
-    // ------------------ Receives pool reference ------------------
     public void SetPool(ObjectPool pool)
     {
         this.pool = pool;
     }
 
-    // ------------------ Trigger detection ------------------
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -44,7 +42,6 @@ public class CollectibleObject : MonoBehaviour, IPlayerCollision, IPoolable
         pool.Return(gameObject);
     }
 
-    // ------------------ Collectible logic ------------------
     public void OnPlayerCollision()
     {
 
